35bee7a082561fdd90fc7ea9b7386f13b7d09af4
SAGAZ <messie.sagaz1990@gmail.com>
2026-06-19 01:19:20 -0600

    feat: calendario enlazado a eventos

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -1,43 +1,76 @@
+import { Link } from "react-router-dom";
+
 import { eventos } from "../data/eventos";
 
 export const Calendario = () => {
   return (
     <section>
       <h1 className="text-4xl font-bold text-slate-800 mb-2">
-        Calendario
+        Calendario Académico
       </h1>
 
       <p className="text-slate-500 mb-8">
-        Próximas fechas importantes.
+        Próximos eventos y actividades programadas.
       </p>
 
-      <div className="bg-white rounded-xl shadow-md p-6">
+      <div className="grid gap-4">
         {eventos.map((evento) => (
-          <div
+          <Link
             key={evento.id}
-            className="
-              flex
-              justify-between
-              items-center
-              py-4
-              border-b
-              last:border-none
-            "
+            to={`/eventos/${evento.id}`}
           >
-            <div>
-              <h2 className="font-semibold">
-                {evento.titulo}
-              </h2>
+            <article
+              className="
+                bg-white
+                p-6
+                rounded-xl
+                shadow-md
+                transition
+                hover:shadow-lg
+                hover:-translate-y-1
+              "
+            >
+              <div
+                className="
+                  flex
+                  flex-col
+                  md:flex-row
+                  md:items-center
+                  md:justify-between
+                  gap-3
+                "
+              >
+                <div>
+                  <h2
+                    className="
+                      text-xl
+                      font-semibold
+                    "
+                  >
+                    {evento.titulo}
+                  </h2>
 
-              <p className="text-sm text-slate-500">
-                {evento.lugar}
-              </p>
-            </div>
+                  <p className="text-slate-500">
+                    📍 {evento.lugar}
+                  </p>
+                </div>
 
-            <span className="font-medium text-[#6A0032]">
-              {evento.fecha}
-            </span>
-          </div>
+                <div
+                  className="
+                    bg-[#6A0032]
+                    text-white
+                    px-4
+                    py-2
+                    rounded-lg
+                    text-center
+                    font-medium
+                  "
+                >
+                  {evento.fecha}
+                </div>
+              </div>
+            </article>
+          </Link>
         ))}
       </div>
     </section>
